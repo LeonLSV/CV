e514e7de9ec8438b62797fdab4c64f9a6d4733d8
Leon Ramiro <racramiro@gmail.com>
2021-02-24 16:12:06 -0500

    [add] scroll trigger

--- src/components/Proyectos/index.js
+++ src/components/Proyectos/index.js
@@ -11,6 +11,10 @@ import estereofund from "../../images/estereofund.png";
 import lsvtools from "../../images/lsvtools.png";
 import efectohaas from "../../images/efectohaas.png";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 const contentStyle = {
   color: "#fff",
   textAlign: "center",
@@ -38,127 +42,157 @@ export const Proyectos = () => {
     [element]
   );
 
+  useEffect(() => {
+    gsap.from(".intro", {
+      duration: 3,
+      y: "-150",
+      opacity: 0,
+      ease: "ease-in",
+      scrollTrigger: {
+        trigger: ".intro",
+        start: "top 80%",
+        end: "top 50%",
+        scrub: "true",
+        markers: "true",
+      },
+    });
+    gsap.to(".intro", {
+      duration: 3,
+      y: "0",
+      opacity: 1,
+      ease: "ease-in",
+      scrollTrigger: {
+        trigger: ".intro",
+        start: "top 80%",
+        end: "top 50%",
+        scrub: "true",
+        markers: "true",
+      },
+    });
+  });
+
   return (
     <>
       <GlobalProyectos>
-        <Row
-          ref={element}
-          style={{
-            // height: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            background: "black",
-          }}>
-          {show && (
-            <>
-              <Col lg={1} xs={0}>
-                <Proyecto />
-              </Col>
-              <Col lg={11} xs={24} style={{ marginTop: "-150px" }}>
-                <Carousel autoplay effect="fade">
-                  <div>
-                    <Tooltip
-                      placement="top"
-                      title="Esta es mi página web oficial. Desde 2015 estoy implementando WordPress. Me encargo tanto del Frontend como del Backend.">
-                      <div style={contentStyle}>
-                        <Card
-                          titulo="León Sonido Virtual"
-                          descripcion="Escuela de audio en vivo. Hecha con WordPress."
-                          hrefImg="https://leonsonidovirtual.com/"
-                          image={lsv}
-                          href="https://leonsonidovirtual.com/"
-                        />
-                      </div>
-                    </Tooltip>
-                  </div>
-                  <div>
-                    <Tooltip
-                      placement="top"
-                      title="Esta aplicación es propia y la cree en 2018 en JAVA. Fue el puntapie para decidir perfeccionar mis habilidades de programador.">
-                      <div style={contentStyle}>
-                        <Card
-                          titulo="LSV Tools - App de escritorio"
-                          descripcion="Aplicación creada y programada por mi."
-                          hrefImg="https://leonsonidovirtual.com/lsv-tools/"
-                          image={lsvtools}
-                          href="https://www.youtube.com/watch?v=EVtRBdwfDuY&t=262s"
-                        />
-                      </div>
-                    </Tooltip>
-                  </div>
-                  <div>
-                    <Tooltip
-                      placement="top"
-                      title="Este cuestionario esta hecho en JavaScript puro. Aprendí como usar las funciones para interactuar con el usuario.">
-                      <div style={contentStyle}>
-                        <Card
-                          titulo="Cuestionario JS"
-                          descripcion="Cuestionario del curso de Ajuste de Sistemas de sonido."
-                          hrefImg="https://leonlsv.github.io/Cuestionario/"
-                          image={cuestionario}
-                          href="https://github.com/LeonLSV/Cuestionario"
-                        />
-                      </div>
-                    </Tooltip>
-                  </div>
-                </Carousel>
-              </Col>
-              <Col lg={11} xs={0} style={{ marginTop: "-150px" }}>
-                <Carousel autoplay effect="fade">
-                  <div>
-                    <Tooltip
-                      placement="top"
-                      title="Esta APP es propia. La cree en 2018 en JAVA y recientemente la pase a React como PWA. Aprendí el uso del Hook useRef y dibujar en base a la información de los inputs.">
-                      <div style={contentStyle}>
-                        <Card
-                          titulo="App Efecto Haas"
-                          descripcion="Aplicación creada y programada por mi (React)."
-                          hrefImg="https://lsv-efecto-haas.vercel.app/ "
-                          image={efectohaas}
-                          href="https://github.com/LeonLSV/EfectoHaas"
-                        />
-                      </div>
-                    </Tooltip>
-                  </div>
+        <div className="intro">
+          <Row
+            ref={element}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              background: "black",
+            }}>
+            {show && (
+              <>
+                <Col lg={1} xs={0}>
+                  <Proyecto />
+                </Col>
+                <Col lg={11} xs={24} style={{ marginTop: "-150px" }}>
+                  <Carousel autoplay effect="fade">
+                    <div>
+                      <Tooltip
+                        placement="top"
+                        title="Esta es mi página web oficial. Desde 2015 estoy implementando WordPress. Me encargo tanto del Frontend como del Backend.">
+                        <div style={contentStyle}>
+                          <Card
+                            titulo="León Sonido Virtual"
+                            descripcion="Escuela de audio en vivo. Hecha con WordPress."
+                            hrefImg="https://leonsonidovirtual.com/"
+                            image={lsv}
+                            href="https://leonsonidovirtual.com/"
+                          />
+                        </div>
+                      </Tooltip>
+                    </div>
+                    <div>
+                      <Tooltip
+                        placement="top"
+                        title="Esta aplicación es propia y la cree en 2018 en JAVA. Fue el puntapie para decidir perfeccionar mis habilidades de programador.">
+                        <div style={contentStyle}>
+                          <Card
+                            titulo="LSV Tools - App de escritorio"
+                            descripcion="Aplicación creada y programada por mi."
+                            hrefImg="https://leonsonidovirtual.com/lsv-tools/"
+                            image={lsvtools}
+                            href="https://www.youtube.com/watch?v=EVtRBdwfDuY&t=262s"
+                          />
+                        </div>
+                      </Tooltip>
+                    </div>
+                    <div>
+                      <Tooltip
+                        placement="top"
+                        title="Este cuestionario esta hecho en JavaScript puro. Aprendí como usar las funciones para interactuar con el usuario.">
+                        <div style={contentStyle}>
+                          <Card
+                            titulo="Cuestionario JS"
+                            descripcion="Cuestionario del curso de Ajuste de Sistemas de sonido."
+                            hrefImg="https://leonlsv.github.io/Cuestionario/"
+                            image={cuestionario}
+                            href="https://github.com/LeonLSV/Cuestionario"
+                          />
+                        </div>
+                      </Tooltip>
+                    </div>
+                  </Carousel>
+                </Col>
+                <Col lg={11} xs={0} style={{ marginTop: "-150px" }}>
+                  <Carousel autoplay effect="fade">
+                    <div>
+                      <Tooltip
+                        placement="top"
+                        title="Esta APP es propia. La cree en 2018 en JAVA y recientemente la pase a React como PWA. Aprendí el uso del Hook useRef y dibujar en base a la información de los inputs.">
+                        <div style={contentStyle}>
+                          <Card
+                            titulo="App Efecto Haas"
+                            descripcion="Aplicación creada y programada por mi (React)."
+                            hrefImg="https://lsv-efecto-haas.vercel.app/ "
+                            image={efectohaas}
+                            href="https://github.com/LeonLSV/EfectoHaas"
+                          />
+                        </div>
+                      </Tooltip>
+                    </div>
 
-                  <div>
-                    <Tooltip
-                      placement="top"
-                      title="Este es uno de los varios trabajos que hice de forma independiente para terceros. Está hecha con WordPress.">
-                      <div style={contentStyle}>
-                        <Card
-                          titulo="Stereo Fund"
-                          descripcion="Página web creada en WordPress."
-                          hrefImg="https://stereofund.com.mx/"
-                          image={estereofund}
-                          href="https://stereofund.com.mx/"
-                        />
-                      </div>
-                    </Tooltip>
-                  </div>
+                    <div>
+                      <Tooltip
+                        placement="top"
+                        title="Este es uno de los varios trabajos que hice de forma independiente para terceros. Está hecha con WordPress.">
+                        <div style={contentStyle}>
+                          <Card
+                            titulo="Stereo Fund"
+                            descripcion="Página web creada en WordPress."
+                            hrefImg="https://stereofund.com.mx/"
+                            image={estereofund}
+                            href="https://stereofund.com.mx/"
+                          />
+                        </div>
+                      </Tooltip>
+                    </div>
 
-                  <div>
-                    <Tooltip
-                      placement="top"
-                      title="Esta App es parte de un curso de Next.js donde aprendí a consumir una API.">
-                      <div style={contentStyle}>
-                        <Card
-                          titulo="Podcasts"
-                          descripcion="Consumo de API de Podcasts."
-                          hrefImg="https://next-js-pi-opal.vercel.app/"
-                          image={podcast}
-                          href="https://github.com/LeonLSV/NextJs"
-                        />
-                      </div>
-                    </Tooltip>
-                  </div>
-                </Carousel>
-              </Col>
-              <Col lg={1} xs={0}></Col>
-            </>
-          )}
-        </Row>
+                    <div>
+                      <Tooltip
+                        placement="top"
+                        title="Esta App es parte de un curso de Next.js donde aprendí a consumir una API.">
+                        <div style={contentStyle}>
+                          <Card
+                            titulo="Podcasts"
+                            descripcion="Consumo de API de Podcasts."
+                            hrefImg="https://next-js-pi-opal.vercel.app/"
+                            image={podcast}
+                            href="https://github.com/LeonLSV/NextJs"
+                          />
+                        </div>
+                      </Tooltip>
+                    </div>
+                  </Carousel>
+                </Col>
+                <Col lg={1} xs={0}></Col>
+              </>
+            )}
+          </Row>
+        </div>
       </GlobalProyectos>
     </>
   );
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   GlobalTitle,
   Parrafo,
@@ -6,21 +6,50 @@ import {
   Title2Header,
   Line,
 } from "./styled";
+import { gsap, TimelineLite, Power3 } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 export const Title = () => {
+  let tl = new TimelineLite({ delay: 0.3 });
+  let tl2 = new TimelineLite({ delay: 0.3 });
+
+  useEffect(() => {
+    tl.staggerFrom(
+      ".animacion1",
+      1,
+      { y: 30, opacity: 0, ease: Power3.easeOut },
+      0.15,
+      "Start"
+    );
+    tl2.staggerFrom(
+      ".animacion2",
+      2,
+      { opacity: 0, ease: Power3.easeOut },
+      0.15,
+      "Start"
+    );
+  });
+
   return (
     <>
       <GlobalTitle>
-        <TitleHeader>Front-End</TitleHeader>
-        <Title2Header>Developer.</Title2Header>
-        <Parrafo>
-          Front-End Developer{" "}
-          <span style={{ color: "#73af5c" }}>
-            enfocado en el desarrollo de aplicaciones web con React.
-          </span>{" "}
-          Pro-activo y dinámico, con capacidad de rápido aprendizaje, creativo y
-          versátil en la adaptación de nuevos entornos y retos.
-        </Parrafo>
+        <div className="animacion1">
+          <TitleHeader>Front-End</TitleHeader>
+        </div>
+        <div className="animacion1">
+          <Title2Header>Developer.</Title2Header>
+        </div>
+        <div className="animacion2">
+          <Parrafo>
+            Front-End Developer{" "}
+            <span style={{ color: "#73af5c" }}>
+              enfocado en el desarrollo de aplicaciones web con React.
+            </span>{" "}
+            Pro-activo y dinámico, con capacidad de rápido aprendizaje, creativo
+            y versátil en la adaptación de nuevos entornos y retos.
+          </Parrafo>
+        </div>
         <Line></Line>
       </GlobalTitle>
     </>
